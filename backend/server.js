2a747fa17e25e8a5757128606463a052a86d2922
Eduardo <edu.vlemes@gmail.com>
2025-08-29 11:58:24 -0300

    manual reset

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(cors({
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path}`);
   next();
-});
+}); 
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
